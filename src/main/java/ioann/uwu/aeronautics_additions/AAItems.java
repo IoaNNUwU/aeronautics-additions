0acff83c10fe7dc197be3826883b532d60873c40
IoaNNUwU <ioannxd@gmail.com>
2026-04-23 01:10:45 +0200

    Add redstone cable item

--- src/main/java/ioann/uwu/aeronautics_additions/AAItems.java
+++ src/main/java/ioann/uwu/aeronautics_additions/AAItems.java
@@ -1,9 +1,38 @@
 package ioann.uwu.aeronautics_additions;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import dev.simulated_team.simulated.index.SimItems;
+import ioann.uwu.aeronautics_additions.items.RedstoneCableItem;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.common.Tags;
 
 public class AAItems {
     private static final CreateRegistrate REGISTRATE = AeronauticsAdditions.getRegistrate();
+
+    public static ItemEntry<RedstoneCableItem> REDSTONE_CABLE = REGISTRATE
+            .item("redstone_cable", RedstoneCableItem::new)
+            .recipe((ctx, recipe) -> ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ctx.get(), 1)
+                    .pattern("RSR")
+                    .pattern("NSN")
+                    .pattern("RSR")
+                    .define('S', Tags.Items.STRINGS)
+                    .define('N', Tags.Items.NUGGETS_IRON)
+                    .define('R', Tags.Items.DUSTS_REDSTONE)
+                    .unlockedBy("has_ingredient", RegistrateRecipeProvider.has(Tags.Items.DUSTS_REDSTONE))
+                    .save(recipe)
+            )
+            .recipe((ctx, recipe) -> ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ctx.get(), 1)
+                    .requires(Items.REDSTONE, 4)
+                    .requires(SimItems.ROPE_COUPLING, 1)
+                    .unlockedBy("has_ingredient", RegistrateRecipeProvider.has(Tags.Items.DUSTS_REDSTONE))
+                    .save(recipe)
+            )
+            .register();
 
     public static void init() {
     }
---
+++ src/main/java/ioann/uwu/aeronautics_additions/items/RedstoneCableItem.java
@@ -0,0 +1,109 @@
+package ioann.uwu.aeronautics_additions.items;
+
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import dev.simulated_team.simulated.data.advancements.SimAdvancements;
+import dev.simulated_team.simulated.index.SimDataComponents;
+import ioann.uwu.aeronautics_additions.blocks.redstone_cable_connector.RedstoneCableHolderBehavior;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class RedstoneCableItem extends Item {
+
+    public static final DataComponentType<BlockPos> CABLE_ORIGIN = SimDataComponents.ROPE_FIRST_CONNECTION;
+
+    public RedstoneCableItem(Properties properties) {
+        super(properties);
+    }
+
+    public static boolean isValidRedstoneCableAttachment(Level level, BlockPos blockPos) {
+        boolean validLocation = false;
+
+        if (level.getBlockEntity(blockPos) instanceof SmartBlockEntity smartBlockEntity) {
+            RedstoneCableHolderBehavior behavior = smartBlockEntity.getBehaviour(RedstoneCableHolderBehavior.TYPE);
+
+            if (behavior != null && !behavior.isAttached()) {
+                validLocation = true;
+            }
+        }
+        return validLocation;
+    }
+
+    public static RedstoneCableHolderBehavior getRedstoneCableHolder(Level level, BlockPos blockPos) {
+        RedstoneCableHolderBehavior holder = null;
+        if (level.getBlockEntity(blockPos) instanceof SmartBlockEntity smartBlockEntity) {
+            RedstoneCableHolderBehavior behavior = smartBlockEntity.getBehaviour(RedstoneCableHolderBehavior.TYPE);
+
+            if (behavior != null) {
+                holder = behavior;
+            }
+        }
+        return holder;
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext ctx) {
+        BlockPos blockPos = ctx.getClickedPos();
+        Level level = ctx.getLevel();
+        ItemStack usedStack = ctx.getItemInHand();
+        Player player = ctx.getPlayer();
+
+        if (player != null && player.isShiftKeyDown()) {
+            usedStack.remove(CABLE_ORIGIN);
+            return InteractionResult.SUCCESS;
+        }
+
+        if (isValidRedstoneCableAttachment(level, blockPos)) {
+
+            if (usedStack.has(CABLE_ORIGIN)) {
+
+                if (!level.isClientSide) {
+                    if (!this.attachRedstoneCable(level, usedStack.get(CABLE_ORIGIN), blockPos)) {
+                        usedStack.remove(CABLE_ORIGIN);
+                        return InteractionResult.SUCCESS;
+                    } else {
+                        SimAdvancements.LEARNING_THE_ROPES.awardTo(player);
+                    }
+                }
+
+                usedStack.remove(CABLE_ORIGIN);
+
+                if (player != null && !player.isCreative()) {
+                    usedStack.shrink(1);
+                }
+
+            } else {
+                usedStack.set(CABLE_ORIGIN, blockPos);
+            }
+            return InteractionResult.SUCCESS;
+        }
+
+        return super.useOn(ctx);
+    }
+
+    private boolean attachRedstoneCable(Level level, BlockPos origin, BlockPos clickedPos) {
+        RedstoneCableHolderBehavior originHolder = getRedstoneCableHolder(level, origin);
+        if (originHolder == null) {
+            return false;
+        }
+
+        RedstoneCableHolderBehavior clickedHolder = getRedstoneCableHolder(level, origin);
+        if (clickedHolder == null) {
+            return false;
+        }
+
+        if (originHolder.createRope(clickedHolder)) {
+            level.playSound(null, origin, SoundEvents.WOOL_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
+            level.playSound(null, origin, SoundEvents.WOOL_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
+            return true;
+        }
+        return false;
+    }
+}
