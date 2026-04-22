658ef4cbc3c3033d178ad4a838112247e6d2c052
IoaNNUwU <ioannxd@gmail.com>
2026-04-22 23:29:39 +0200

    Split redstone cable connector from rope connector

--- src/main/java/ioann/uwu/aeronautics_additions/blocks/redstone_cable_connector/RedstoneCableConnectorBlock.java
+++ src/main/java/ioann/uwu/aeronautics_additions/blocks/redstone_cable_connector/RedstoneCableConnectorBlock.java
@@ -1,34 +1,77 @@
 package ioann.uwu.aeronautics_additions.blocks.redstone_cable_connector;
 
+import com.mojang.serialization.MapCodec;
+import com.simibubi.create.api.contraption.BlockMovementChecks;
+import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.impl.contraption.BlockMovementChecksImpl;
+import dev.ryanhcode.sable.api.block.BlockSubLevelAssemblyListener;
+import dev.ryanhcode.sable.api.block.BlockSubLevelCollisionShape;
+import dev.simulated_team.simulated.content.blocks.rope.RopeHolderBlock;
 import dev.simulated_team.simulated.content.blocks.rope.rope_connector.RopeConnectorBlock;
-import dev.simulated_team.simulated.content.blocks.rope.rope_connector.RopeConnectorBlockEntity;
+import dev.simulated_team.simulated.content.blocks.util.AbstractDirectionalAxisBlock;
 import dev.simulated_team.simulated.index.SimBlockShapes;
+import dev.simulated_team.simulated.index.SimTags;
 import dev.simulated_team.simulated.util.DirectionalAxisShaper;
 import ioann.uwu.aeronautics_additions.AABlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class RedstoneCableConnectorBlock extends RopeConnectorBlock {
+public class RedstoneCableConnectorBlock extends AbstractDirectionalAxisBlock implements
+        IBE<RedstoneCableConnectorBlockEntity>,
+        RopeHolderBlock<RedstoneCableConnectorBlockEntity>,
+        BlockSubLevelAssemblyListener,
+        BlockSubLevelCollisionShape
+{
 
     public static final IntegerProperty POWER = RedStoneWireBlock.POWER;
+
+    static {
+        BlockMovementChecksImpl.registerAttachedCheck(
+                (state, world, pos, direction) -> {
+                    final BlockState relativeState = world.getBlockState(pos.relative(direction));
+                    if (state.getBlock() instanceof RedstoneCableConnectorBlock && state.getValue(RopeConnectorBlock.FACING) == direction.getOpposite()) {
+                        return BlockMovementChecks.CheckResult.SUCCESS;
+                    }
+                    if (relativeState.getBlock() instanceof RedstoneCableConnectorBlock && relativeState.getValue(RopeConnectorBlock.FACING) == direction) {
+                        return BlockMovementChecks.CheckResult.SUCCESS;
+                    }
+                    return BlockMovementChecks.CheckResult.PASS;
+                }
+        );
+    }
 
     private boolean shouldSignal = true;
 
     public RedstoneCableConnectorBlock(Properties properties) {
         super(properties);
         registerDefaultState(defaultBlockState().setValue(POWER, 0));
+    }
+
+    public static final MapCodec<RedstoneCableConnectorBlock> CODEC = simpleCodec(RedstoneCableConnectorBlock::new);
+
+    @Override
+    protected MapCodec<? extends DirectionalBlock> codec() {
+        return CODEC;
     }
 
     private static final DirectionalAxisShaper SHAPE = DirectionalAxisShaper.make(
@@ -42,14 +85,53 @@ public class RedstoneCableConnectorBlock extends RopeConnectorBlock {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        super.createBlockStateDefinition(pBuilder);
-        pBuilder.add(POWER);
+    public VoxelShape getSubLevelCollisionShape(BlockGetter blockGetter, BlockState state) {
+        // TODO: Add simpler Physics Collider
+        return SHAPE.get(state.getValue(FACING), state.getValue(AXIS_ALONG_FIRST_COORDINATE));
     }
 
     @Override
-    public BlockEntityType<? extends RopeConnectorBlockEntity> getBlockEntityType() {
+    protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
+        super.onPlace(state, level, pos, oldState, movedByPiston);
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        IBE.onRemove(state, level, pos, newState);
+    }
+
+    @Override
+    public Class<RedstoneCableConnectorBlockEntity> getBlockEntityClass() {
+        return RedstoneCableConnectorBlockEntity.class;
+    }
+
+    @Override
+    public BlockEntityType<? extends RedstoneCableConnectorBlockEntity> getBlockEntityType() {
         return AABlockEntityTypes.REDSTONE_CABLE_CONNECTOR.get();
+    }
+
+    @Override
+    protected Direction getFacingForPlacement(BlockPlaceContext context) {
+        return context.getClickedFace();
+    }
+
+    @Override
+    protected boolean getAxisAlignmentForPlacement(BlockPlaceContext context) {
+        return context.getHorizontalDirection().getAxis() != Direction.Axis.X;
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(final ItemStack stack, final BlockState state, final Level level, final BlockPos pos, final Player player, final InteractionHand hand, final BlockHitResult hitResult) {
+        if (!level.isClientSide() && stack.is(SimTags.Items.DESTROYS_ROPE)) {
+            return RopeHolderBlock.shearRope(this, level, pos, (ServerPlayer) player);
+        }
+        return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        super.createBlockStateDefinition(pBuilder);
+        pBuilder.add(POWER);
     }
 
     @Override
--- src/main/java/ioann/uwu/aeronautics_additions/blocks/redstone_cable_connector/RedstoneCableConnectorBlockEntity.java
+++ src/main/java/ioann/uwu/aeronautics_additions/blocks/redstone_cable_connector/RedstoneCableConnectorBlockEntity.java
@@ -1,10 +1,16 @@
 package ioann.uwu.aeronautics_additions.blocks.redstone_cable_connector;
 
 import com.google.common.collect.Sets;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import dev.ryanhcode.sable.api.sublevel.ServerSubLevelContainer;
 import dev.ryanhcode.sable.api.sublevel.SubLevelContainer;
 import dev.ryanhcode.sable.sublevel.SubLevel;
+import dev.simulated_team.simulated.content.blocks.rope.RopeStrandHolderBehavior;
+import dev.simulated_team.simulated.content.blocks.rope.RopeStrandHolderBlockEntity;
+import dev.simulated_team.simulated.content.blocks.rope.rope_connector.RopeConnectorBlock;
 import dev.simulated_team.simulated.content.blocks.rope.rope_connector.RopeConnectorBlockEntity;
+import dev.simulated_team.simulated.content.blocks.rope.strand.client.ClientRopeStrand;
 import dev.simulated_team.simulated.content.blocks.rope.strand.server.RopeAttachment;
 import dev.simulated_team.simulated.content.blocks.rope.strand.server.RopeAttachmentPoint;
 import dev.simulated_team.simulated.content.blocks.rope.strand.server.ServerRopeStrand;
@@ -15,13 +21,74 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
+import java.util.List;
 import java.util.Set;
 
-public class RedstoneCableConnectorBlockEntity extends RopeConnectorBlockEntity {
+public class RedstoneCableConnectorBlockEntity extends SmartBlockEntity implements RopeStrandHolderBlockEntity {
+
+    public static final double RENDER_BOUNDING_BOX_INFLATION = 3.0;
+
+    private RopeStrandHolderBehavior ropeHolder;
 
     public RedstoneCableConnectorBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    public RopeStrandHolderBehavior getRopeHolder() {
+        return this.ropeHolder;
+    }
+
+    @Override
+    public void addBehaviours(final List<BlockEntityBehaviour> behaviours) {
+        behaviours.add(this.ropeHolder = new RopeStrandHolderBehavior(this));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.level.isClientSide) {
+            this.invalidateRenderBoundingBox();
+        }
+    }
+
+    @Override
+    public AABB getRenderBoundingBox() {
+        final ClientRopeStrand rope = this.ropeHolder.getClientStrand();
+        if (rope != null && this.ropeHolder.ownsRope()) {
+            final AABB bounds = rope.getBounds();
+
+            if (bounds == null) {
+                return super.getRenderBoundingBox();
+            }
+
+            return bounds.inflate(RENDER_BOUNDING_BOX_INFLATION);
+        } else {
+            return super.getRenderBoundingBox();
+        }
+    }
+
+    @Override
+    public RopeStrandHolderBehavior getBehavior() {
+        return this.ropeHolder;
+    }
+
+    @Override
+    public Vec3 getAttachmentPoint(final BlockPos pos, final BlockState state) {
+        final Direction facing = state.getValue(RopeConnectorBlock.FACING);
+        final double offset = -3.0 / 16.0;
+
+        return pos.getCenter().add(facing.getStepX() * offset, facing.getStepY() * offset, facing.getStepZ() * offset);
+    }
+
+    public Vec3 getVisualAttachmentPoint(final BlockPos pos, final BlockState state) {
+        final Direction facing = state.getValue(RopeConnectorBlock.FACING);
+        final double offset = -4.0 / 16.0;
+
+        return pos.getCenter().add(facing.getStepX() * offset, facing.getStepY() * offset, facing.getStepZ() * offset);
     }
 
     public void updateOtherSide(int newSignalLevel, Block block, ServerLevel level) {
